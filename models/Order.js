b8968029a82a2459c269682010872ede12dddd1d
abhishekkbxr <abhishekbxr203@gmail.com>
2022-08-24 23:32:03 +0530

    Ecommerce

---
+++ models/Order.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    products: [{
+        productId: { type: String },
+        quantity: { type: Number, default: 1 }
+    }],
+    address: { type: String, required: true },
+    amount: { type: Number, required: true },
+    status: { type: String, default: "Panding", required: true },
+
+}, { timestamps: true });
+
+export default mongoose.models.Order || mongoose.model("Order", OrderSchema);
